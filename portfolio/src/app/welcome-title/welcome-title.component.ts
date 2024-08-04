3069e3d8833203bd21098016d32dc5746f738710
fhenriques <fhenriques@pictet-technologies.com>
2024-08-04 22:57:48 +0100

    Starting portfolio with face and welcome screen

---
+++ portfolio/src/app/welcome-title/welcome-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-title.component.html',
+  styleUrl: './welcome-title.component.css'
+})
+export class WelcomeTitleComponent {
+
+}
