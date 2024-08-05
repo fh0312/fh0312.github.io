6bd124efa35269d5f034394a865ca3d36cc2a92d
fhenriques <fhenriques@pictet-technologies.com>
2024-08-05 22:32:50 +0100

    Changing header and buttons sizing + ref to welcome button

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -9,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  scrollToEvent($event: MouseEvent) {
+    const button = $event.target as HTMLElement;
+
+    const id = button.className;
+    console.log("id", id);
+    console.log(document);
+    const element = document.getElementById(id) as HTMLElement;
+    console.log("element", element);
+    element.scrollIntoView({ behavior: 'smooth' });
+
+  }
 }
