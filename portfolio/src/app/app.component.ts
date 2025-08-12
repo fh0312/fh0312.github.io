f96d33dbd6437475d43be6d8a0bda0773008725b
fh0312 <02francisco.henriques@gmail.com>
2025-08-12 01:32:17 +0100

    Adding about me section including svg icons. Format only for pcs (mobile tbd)

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {MySelfComponent} from "./myself/myself.component";
+import {AboutMeComponent} from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, MySelfComponent],
+  imports: [HeaderComponent, FooterComponent, MySelfComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
